0e954c5fcb3878558db623889d5a969e2724c8f6
hamletshu <ahngo13@naver.com>
2020-05-15 02:06:30 +0900

    modify structure Header.jsx

--- MyDID/src/Header.jsx
+++ MyDID/src/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Button, Image } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
@@ -7,24 +7,24 @@ import {} from "jquery.cookie";
 axios.defaults.withCredentials = true;
 const headers = { withCredentials: true };
 
-class Header extends Component {
-  state = {
-    buttonDisplay: "none"
-  };
+function Header(){
 
-  componentDidMount() {
+  const [buttonDisplay, setButtonDisplay] = useState("none");
+  
+  useEffect(() => {
+    getButtonStyle();
+  }, []);
+
+  function getButtonStyle(){
     if ($.cookie("login_id")) {
-      this.setState({
-        buttonDisplay: "block"
-      });
+      setButtonDisplay("block");
     } else {
-      this.setState({
-        buttonDisplay: "none"
-      });
+      setButtonDisplay("none");
     }
   }
+  
 
-  logout = () => {
+  function logout(){
     axios
       .get("http://localhost:8080/member/logout", {
         headers
@@ -37,42 +37,42 @@ class Header extends Component {
         }
       });
   };
-  render() {
-    const buttonStyle = {
-      margin: "0px 5px 0px 10px",
-      display: this.state.buttonDisplay
-    };
 
-    return (
-      <div>
-        <Navbar>
-          <Navbar.Brand href="/">Today I Learned</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <NavLink to="/mypage">
-              <Button style={buttonStyle} variant="primary">
-                회원정보 수정
-              </Button>
-            </NavLink>
-            <NavLink to="/">
-              <Button style={buttonStyle} variant="primary">
-                글목록
-              </Button>
-            </NavLink>
-            <NavLink to="/boardWrite">
-              <Button style={buttonStyle} variant="primary">
-                글쓰기
-              </Button>
-            </NavLink>
-            <Button style={buttonStyle} onClick={this.logout} variant="primary">
-              로그아웃
+  const buttonStyle = {
+    margin: "0px 5px 0px 10px",
+    display: buttonDisplay
+  };
+
+  return (
+    <div>
+      <Navbar>
+        <Navbar.Brand href="/">Today I Learned</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <NavLink to="/mypage">
+            <Button style={buttonStyle} variant="primary">
+              회원정보 수정
             </Button>
-          </Navbar.Collapse>
-        </Navbar>
-        <Image src="./img/main.png" fluid />
-      </div>
-    );
-  }
+          </NavLink>
+          <NavLink to="/">
+            <Button style={buttonStyle} variant="primary">
+              글목록
+            </Button>
+          </NavLink>
+          <NavLink to="/boardWrite">
+            <Button style={buttonStyle} variant="primary">
+              글쓰기
+            </Button>
+          </NavLink>
+          <Button style={buttonStyle} onClick={logout} variant="primary">
+            로그아웃
+          </Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Image src="./img/main.png" fluid />
+    </div>
+  );
+  
 }
 
 export default Header;
